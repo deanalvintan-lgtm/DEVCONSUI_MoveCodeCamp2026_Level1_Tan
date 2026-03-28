83f41c026baad2cef036da0e62348972029f14ab
root <root@CL5-03.cl5.lab>
2026-03-28 16:38:28 +0800

    Updated imports and frontend files

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0xd422d8037920997c8a416b3bc457fb7b24ff6bbd5a444152f8b84835d21e919d";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
